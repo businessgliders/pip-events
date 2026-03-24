ab4eca7910ea03ff2dc962d5c4575d1f9ad1c601
Pilates in Pink Studio <info@pilatesinpinkstudio.com>
2026-03-24 13:10:23 +0000

    File changes

--- base44/functions/sendEventEmails/entry.ts
+++ base44/functions/sendEventEmails/entry.ts
@@ -2,7 +2,7 @@ import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 const RESEND_API_KEY = Deno.env.get('RESEND_API_KEY');
 const OWNER_EMAIL = 'info@pilatesinpinkstudio.com';
-const FROM_EMAIL = 'Pilates in Pink™ Studio <events@pilatesinpinkstudio.com>';
+const FROM_EMAIL = 'Pilates in Pink™ Studio <events@pilatesinpink.ca>';
 
 async function sendEmail({ to, subject, html }) {
   const res = await fetch('https://api.resend.com/emails', {
